8d6446eba2ee8b20f7df7711344602402f03eb35
Gilbert Irumva <gilbert@opengates.app>
2026-04-08 11:28:39 +0300

    completed part2 exercise 2.8 {phoneBook step 3}

--- phoneBook/step1/src/App.jsx
+++ phoneBook/step1/src/App.jsx
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' } ])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '+1 401 300 812' }
+  ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewName = (event) => {
     event.preventDefault()
+
+    // check duplicate name (case-insensitive)
     const exists = persons.some(
       person => person.name.toLowerCase() === newName.toLowerCase()
     )
+
     if (exists) {
-      alert(`${newName} already exists to the phoneBook`)
+      alert(`${newName} already exists`)
+      return
     }
-    setPersons([...persons, { name: newName }])
+
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons([...persons, newPerson])
     setNewName('')
+    setNewNumber('')
   }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -25,17 +46,22 @@ const App = () => {
       <form onSubmit={addNewName}>
         <div>
           Name:
-          <input
-            value={newName}
-            onChange={handleNameChange}
-          />
+          <input value={newName} onChange={handleNameChange}/>
+        </div>
+
+        <div>
+          Number:
+          <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <button type="submit">Add</button>
       </form>
-      <h2>Members</h2>
+
+      <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
     </div>
